bb7cb6f18c24684531e1e4968734cb58cbdb1b08
atulit gaur <atulit23@gmail.com>
2024-02-06 14:07:46 +0530

    latest changes

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
+from flask import Flask, jsonify
 import scapy.all as scapy
-import subprocess
 import time
 import socket
+from threading import Thread
 
 def get_ip():
     try:
-        host_name = socket.gethostname()
-        ip_address = socket.gethostbyname(host_name)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
         return ip_address
     except Exception as e:
         print(f"Error: {e}")
@@ -14,10 +17,12 @@ def get_ip():
 
 
 current_ip = get_ip()
+
 last_packet_time = [time.time()]
 src_size = [0]
 dst_size = [0]
 packet_dict = {'sent': {}, 'received': {}}
+data = {}
 
 def get_protocol_name(protocol_num):
     protocol_names = {
@@ -69,6 +74,7 @@ def get_protocol_name(protocol_num):
     }
     return protocol_names[protocol_num]
 
+
 def update_packet_dict(origin, key, detail):
     sentKeys = list(packet_dict['sent'].keys())
     receivedKeys = list(packet_dict['received'].keys())
@@ -84,37 +90,66 @@ def update_packet_dict(origin, key, detail):
             packet_dict['received'][key]['count'] = packet_dict['received'][key]['count'] + 1
             packet_dict['received'][key]['sentFrom'] = detail
         else:
-            packet_dict['received'].setdefault(key, {'count': 1, 'sentFrom': detail})
+            packet_dict['received'].setdefault(
+                key, {'count': 1, 'sentFrom': detail})
+
 
 def packet_callback(packet):
+    global data
     current_time = time.time()
     if packet.haslayer(scapy.IP):
         src_ip = packet[scapy.IP].src
         dst_ip = packet[scapy.IP].dst
         protocol = packet[scapy.IP].proto
-        # print(packet[scapy.IP])
 
         if current_ip:
             if src_ip == current_ip:
                 src_size[0] = len(packet)
-                update_packet_dict('src', src_ip, dst_ip) #sent packets
+                update_packet_dict('src', src_ip, dst_ip)  # sent packets
             elif dst_ip == current_ip:
-                dst_size[0]= len(packet)
-                update_packet_dict('dst', src_ip, dst_ip) #recieved packets
+                dst_size[0] = len(packet)
+                update_packet_dict('dst', src_ip, dst_ip)  # recieved packets
         else:
             print("Unable to retrieve the IP address.")
 
         time_elasped = current_time - last_packet_time[0]
 
-        print(f"Packet: {src_ip} -> {dst_ip}, Source Size: {src_size[0]} bytes, Sent packets: {packet_dict['sent']}, Received packets: {packet_dict['received']}, Time Elasped: {time_elasped}, Protocol: {get_protocol_name(protocol)}, Destination Size: {dst_size[0]} bytes")
-
+        # data.append("Packet: {src_ip} -> {dst_ip}, Source Size: {src_size[0]} bytes, Sent packets: {packet_dict['sent']}, Received packets: {packet_dict['received']}, Time Elasped: {time_elasped}, Protocol: {get_protocol_name(protocol)}, Destination Size: {dst_size[0]} bytes")
+        data = {
+            'src_ip': src_ip,
+            # 'dst_ip': dst_ip,
+            'src_size': tuple(src_size),  # Convert list to tuple
+            'sent_packets': packet_dict['sent'],
+            'received_packets': packet_dict['received'],
+            'time_elapsed': time_elasped,
+            'protocol': get_protocol_name(protocol),
+            'dst_size': dst_size[0] 
+            
+        }
         last_packet_time[0] = current_time
 
 
-def sniff_packets():
-    scapy.sniff(iface=network_interface, store=False, prn=packet_callback, count=packet_count, promisc=True)
+def sniff_packets(network_interface, packet_count):
+    scapy.sniff(iface=network_interface, store=False,
+                prn=packet_callback, count=packet_count, promisc=True)
+
+
+app = Flask(__name__)
+
+@app.route('/packet_info', methods=['GET'])
+def get_packet_info():
+    return jsonify(data)
+
+
+@app.route('/current_ip', methods=['GET'])
+def get_current_ip():
+    return jsonify({'current_ip': current_ip})
 
 network_interface = "Wi-Fi"
-packet_count = 0 # count -> 0 for conitunous packet collection
+packet_count = 0  # count -> 0 for continuous packet collection
+sniff_thread = Thread(target=sniff_packets, args=(
+    network_interface, packet_count))
+sniff_thread.start()
 
-sniff_packets(network_interface, packet_count)
+if __name__ == '__main__':
+    app.run(debug=True)
